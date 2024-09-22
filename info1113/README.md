ee19f299c98428217fb31af946c95316e410bc1f
lucaliao <lucaliao@tencent.com>
2024-09-22 17:36:03 +0800

    update

---
+++ info1113/README.md
@@ -0,0 +1,16 @@
+java基础语法，前面的task跟着课程内容学习都能完成，最复杂的一个作业是每年都会让用processing库写一个完整的游戏，需要花费大量时间才能完成
+
+******
+本人有多年留学生计算机相关课程的作业代写和辅导经验，支持作业、考试辅导、代写，具体可以加微信**lxhao580**，老师直接接单，不经过中介平台，价格优惠，服务靠谱
+<br/>
+**本人可以免费提供往年考试真题，免费提供课程学习指导**
+<br/>
+
+**接过的部分课程**(可以直接在页面上ctrl + f搜索)
+<br/>
+CIS6930,COMP10002,COMP20003,COMP2041,COMP30023,COMP3506,COMP775,CS0019,EMS5730,ENGG1811,INFS1603,INFS3208,INFS5710,INFS7450,INFS7450,MIE1628,comp10001,comp1110,comp1112,comp1113,comp1730,comp2322,comp3231,comp6730,comp90015,comp9003,comp9021,comp9024,comp9044,comp9321,comp9517,compe9313,csit115,csse2002,csse7023,data2001,data3404 ,dsci553,fins3646,fins3646,fins5546,fit1045,fit1051,fit2004,fit9136,ielec5618,info90002,mie1628,mtrx3760
+
+<br/>
+部分接单记录:
+![接单记录](../images/orders.png)
+*******
---
+++ replace.py
@@ -0,0 +1,27 @@
+import os
+
+# 遍历目录
+for root, dirs, files in os.walk("./"):
+    for file in files:
+        file_path = os.path.join(root, file)
+        if not file.endswith('.md'):
+            continue
+        lines = open(file_path, encoding='utf8').readlines()
+
+        if '******\n' not in lines:
+            print(f'skip {file_path}')
+            continue
+
+        start = lines.index('******\n')
+        end = lines.index('*******\n')
+        common_lines = open('README.md').readlines()
+        if start >= end:
+            print(f'skip {file_path}')
+            continue
+        print(f'processing {file_path}')
+        lines = lines[:start + 1] + common_lines + lines[end:]
+        file = open(file_path, 'w')
+        for line in lines:
+            line = line.replace('images/orders.png', '../images/orders.png')
+            file.write(line)
+        file.close()
